1f72ee50a220f0ddbf9713de70807bf4a56e9a9f
Lauren Keenan <19015831@uhi.ac.uk>
2025-10-18 14:53:12 +0100

    create userconsent sql

---
+++ Data/UserConsentDb.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiScreenTime.Data
+{
+    public class UserConsentDb
+    {
+        public bool IsGranted { get; set; }
+        public DateTime GrantedAt { get; set; }
+        public string Version { get; set; } // Track consent version for GDPR compliance
+        public DateTime? RevokedAt { get; set; }
+
+        public int DataRetentionDays = 60;
+    }
+
+    public class ConsentDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public ConsentDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<UserConsentDb>().Wait();
+        }
+
+        // Revisit this because idk
+        public async Task<bool> HasConsent()
+        {
+            var consent = await _database.Table<UserConsentDb>()
+                .OrderByDescending(c => c.GrantedAt)
+                .FirstOrDefaultAsync();
+
+            return consent?.IsGranted ?? false;
+        }
+
+        public async Task GrantConsent(string version = "1.0")
+        {
+            await _database.InsertAsync(new UserConsentDb
+            {
+                IsGranted = true,
+                GrantedAt = DateTime.UtcNow,
+                Version = version
+            });
+        }
+
+        public async Task RevokeConsent()
+        {
+            var consent = await _database.Table<UserConsentDb>()
+                .OrderByDescending(c => c.GrantedAt)
+                .FirstOrDefaultAsync();
+
+            if (consent != null)
+            {
+                consent.RevokedAt = DateTime.UtcNow;
+                await _database.UpdateAsync(consent);
+            }
+        }
+
+        public async Task<List<UserConsentDb>> GetConsentHistory()
+        {
+            return await _database.Table<UserConsentDb>().ToListAsync();
+        }
+
+        public async Task DeleteAllConsents()
+        {
+            await _database.DeleteAllAsync<UserConsentDb>();
+        }
+    }
+}
