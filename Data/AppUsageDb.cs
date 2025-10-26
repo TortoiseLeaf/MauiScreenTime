4c115c700d0839b410f5d68003985b50dad014a3
Marta Grazia <23011076@uhi.ac.uk>
2025-10-26 11:27:42 +0000

    added app usage data class

---
+++ Data/AppUsageDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiScreenTime.Data
+{
+    public class AppUsageDb
+    {
+        public int Id {  get; set; }
+        public string PackageName { get; set; }
+        public DateTime Date {  get; set; }
+        public long UsageTimeMilliseconds { get; set; }
+    }
+}
