eba23f3b0aab5b1bbdbd5b2754d3c4b7b038b71f
Lauren Keenan <19015831@uhi.ac.uk>
2025-11-25 14:58:35 +0000

    create StartupService

---
+++ Services/IStartupService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiScreenTime.Services
+{
+    public interface IStartupService
+    {
+        Task InitializeAsync();
+        //Task NavigateToAsync(string Route);
+
+    }
+}
---
+++ Services/StartupService.cs
@@ -0,0 +1,34 @@
+﻿using MauiScreenTime.Data;
+using MauiScreenTime.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiScreenTime.Services
+{
+
+    public class StartupService : IStartupService
+    {
+        private readonly ConsentDatabase _consentDatabase;
+
+        public async Task InitializeAsync()
+        {
+            var hasConsent = await _consentDatabase.HasConsent();
+
+            if (_consentDatabase == null)
+            {
+                await Shell.Current.GoToAsync("//ConsentPage");
+            }
+            if (hasConsent)
+            {
+                await Shell.Current.GoToAsync("//DashboardPage");
+            }
+            else
+            {
+                await Shell.Current.GoToAsync("//ConsentPage");
+            }
+        }
+    }
+}
