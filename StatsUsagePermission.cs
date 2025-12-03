8c5d364c4af5ddaa27de6d5f206114dbbd5dc41f
Lauren Keenan <19015831@uhi.ac.uk>
2025-12-03 17:54:04 +0000

    create custom permission class

---
+++ StatsUsagePermission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace MauiScreenTime
+{
+    internal class StatsUsagePermission : BasePlatformPermission
+    {
+#if ANDROID
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions => 
+            new List<(string permission, bool isRuntime)>
+            {
+                (Android.Manifest.Permission.PackageUsageStats, false),
+            }.ToArray();
+
+#endif
+    }
+}
