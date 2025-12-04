a56f5a2c85398460ee232274979fdee7ec44afc7
Marta Grazia <airwar90@gmail.com>
2025-12-04 13:46:56 +0000

    testing restructure

---
+++ src/MauiScreenTime/ViewModels/TestingBinding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiScreenTime.ViewModels
+{
+    public class TestingBinding
+    {
+        private bool isAccessible = true;
+
+        public bool GetIsAccessible { get { return isAccessible; } }
+    }
+}
