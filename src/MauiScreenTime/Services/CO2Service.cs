4b2b7aaa21aea364f585a2e02524480c74f845cc
Lauren Keenan <19015831@uhi.ac.uk>
2026-02-05 15:56:07 +0000

    create CO2 service

---
+++ src/MauiScreenTime/Services/CO2Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiScreenTime.Data;
+
+namespace MauiScreenTime.Services
+{
+    internal class CO2Service : ICO2Service
+    {
+            private readonly ConversionTableDatabase _conversionTableDatabase;
+            private readonly AppUsageDatabase _appUsageDatabase;
+
+
+        public CO2Service(ConversionTableDatabase conversionTableDatabase, AppUsageDatabase appUsageDatabase)
+        {
+            // how much is superfluous, is it worth having the null check? What message or alternative can the else do?
+            if (conversionTableDatabase != null)
+            {
+                _conversionTableDatabase = conversionTableDatabase;
+            }
+
+            _appUsageDatabase = appUsageDatabase;
+        }
+        public async Task CalculateCO2eAsync()
+            {
+            
+                
+            }
+        }
+    }
