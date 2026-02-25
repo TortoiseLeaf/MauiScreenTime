c07a97a62a80f9a19edbf485ec9241766102fb43
Lauren Keenan <19015831@uhi.ac.uk>
2026-02-25 13:20:24 +0000

    delay logic for workmanager

---
+++ src/MauiScreenTime/Helpers/ScheduleCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiScreenTime.Helpers
+{
+    public static class ScheduleCalculator
+    {
+        public static TimeSpan CalculateDelayUntilNextRun(DateTime now, int hour)
+        {
+            var nextRun = now.Date.AddHours(hour);
+
+            if (now >= nextRun)
+                nextRun = nextRun.AddDays(1);
+
+            return nextRun - now;
+        }
+    }
+}
