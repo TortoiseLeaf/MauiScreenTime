ce21e92a18e321e6913ab38e3cc67ef914479a67
Lauren Keenan <19015831@uhi.ac.uk>
2026-03-06 18:49:43 +0000

    comment out workmanager

--- src/MauiScreenTime/Platforms/Android/DailyWorker.cs
+++ src/MauiScreenTime/Platforms/Android/DailyWorker.cs
@@ -1,54 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Android.Content;
-using AndroidX.Work;
-using MauiScreenTime.Services.Interfaces;
-using Android.Util;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Android.Content;
+//using AndroidX.Work;
+//using MauiScreenTime.Services.Interfaces;
+//using Android.Util;
 
-namespace MauiScreenTime.Platforms.Android
-{
-    public class DailyWorker : Worker
-    {
+//namespace MauiScreenTime.Platforms.Android
+//{
+//    public class DailyWorker : Worker
+//    {
        
-        private readonly IDailyWorkerService _dailyWorkerService;
+//        private readonly IDailyWorkerService _dailyWorkerService;
 
-        public DailyWorker(Context context, WorkerParameters parameters)
-        : base(context, parameters)
-        {
-            _dailyWorkerService = null;
-        }
+//        public DailyWorker(Context context, WorkerParameters parameters)
+//        : base(context, parameters)
+//        {
+//            _dailyWorkerService = null;
+//        }
 
-        public DailyWorker(Context context, WorkerParameters parameters, IDailyWorkerService dailyWorkerService)
-            : base(context, parameters)
-        {
+//        public DailyWorker(Context context, WorkerParameters parameters, IDailyWorkerService dailyWorkerService)
+//            : base(context, parameters)
+//        {
             
-            _dailyWorkerService = dailyWorkerService;
-        }
+//            _dailyWorkerService = dailyWorkerService;
+//        }
 
-        public override Result DoWork()
-        {
-            Log.Debug("DailyWorker", "here successfully fired the daily worker");
-            try
-            {
+//        public override Result DoWork()
+//        {
+//            Log.Debug("DailyWorker", "here successfully fired the daily worker");
+//            try
+//            {
                 
 
-                _dailyWorkerService.StoreCO2TotalTodayAsync().GetAwaiter().GetResult();
+//                _dailyWorkerService.StoreCO2TotalTodayAsync().GetAwaiter().GetResult();
 
-                Log.Debug("DailyWorker", "here successfully fired the 23h total congrats");
-                System.Diagnostics.Debug.WriteLine("here Successfully fired the 23h co2 Total congratulations");
-                return Result.InvokeSuccess();
-            }
-            catch (Exception ex)
-            {
-                Log.Debug("DailyWorker", "Error, failed to fire the 23h co2Total");
-                Log.Debug("DailyWorker", ex.Message);
-                Log.Debug("DailyWorker", ex.StackTrace);
+//                Log.Debug("DailyWorker", "here successfully fired the 23h total congrats");
+//                System.Diagnostics.Debug.WriteLine("here Successfully fired the 23h co2 Total congratulations");
+//                return Result.InvokeSuccess();
+//            }
+//            catch (Exception ex)
+//            {
+//                Log.Debug("DailyWorker", "Error, failed to fire the 23h co2Total");
+//                Log.Debug("DailyWorker", ex.Message);
+//                Log.Debug("DailyWorker", ex.StackTrace);
 
-                return Result.InvokeFailure();
-            }
-        }
-    }
-}
+//                return Result.InvokeFailure();
+//            }
+//        }
+//    }
+//}
--- src/MauiScreenTime/Platforms/Android/WorkManagerHelper.cs
+++ src/MauiScreenTime/Platforms/Android/WorkManagerHelper.cs
@@ -1,64 +1,64 @@
-﻿using Android.Content;
-using AndroidX.Work;
-using global::Android.Content;
-using MauiScreenTime.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Android.Content;
+//using AndroidX.Work;
+//using global::Android.Content;
+//using MauiScreenTime.Helpers;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace MauiScreenTime.Platforms.Android
-{
+//namespace MauiScreenTime.Platforms.Android
+//{
     
-    public static class WorkManagerHelper
-    {
-        public static void ScheduleCalculateTotalCO2(Context context)
-        {
-            //var now = DateTime.Now;
-            //var nextRun = DateTime.Today.AddHours(23);
+//    public static class WorkManagerHelper
+//    {
+//        public static void ScheduleCalculateTotalCO2(Context context)
+//        {
+//            //var now = DateTime.Now;
+//            //var nextRun = DateTime.Today.AddHours(23);
 
-            //if (now > nextRun)
-            //    nextRun = nextRun.AddDays(1);
+//            //if (now > nextRun)
+//            //    nextRun = nextRun.AddDays(1);
 
-            //var delay = nextRun - now;
+//            //var delay = nextRun - now;
 
-            var delay = ScheduleCalculator.CalculateDelayUntilNextRun(DateTime.Now, 23);
+//            var delay = ScheduleCalculator.CalculateDelayUntilNextRun(DateTime.Now, 23);
 
-            var constraints = new Constraints.Builder()
-                .SetRequiredNetworkType(NetworkType.NotRequired)
-                .Build();
+//            var constraints = new Constraints.Builder()
+//                .SetRequiredNetworkType(NetworkType.NotRequired)
+//                .Build();
 
-            //var workRequest = (PeriodicWorkRequest)new PeriodicWorkRequest.Builder(
-            //        Java.Lang.Class.FromType(typeof(DailyWorker)),
-            //        15,
-            //        Java.Util.Concurrent.TimeUnit.Minutes!)
-            //    //.SetInitialDelay((long)delay.TotalMinutes, Java.Util.Concurrent.TimeUnit.Minutes!)
-            //    .SetConstraints(constraints)
-            //    .Build();
+//            //var workRequest = (PeriodicWorkRequest)new PeriodicWorkRequest.Builder(
+//            //        Java.Lang.Class.FromType(typeof(DailyWorker)),
+//            //        15,
+//            //        Java.Util.Concurrent.TimeUnit.Minutes!)
+//            //    //.SetInitialDelay((long)delay.TotalMinutes, Java.Util.Concurrent.TimeUnit.Minutes!)
+//            //    .SetConstraints(constraints)
+//            //    .Build();
 
-            //WorkManager
-            //    .GetInstance(context)
-            //    .EnqueueUniquePeriodicWork(
-            //        "total_CO2_23h",
-            //        ExistingPeriodicWorkPolicy.CancelAndReenqueue,
-            //        workRequest);
+//            //WorkManager
+//            //    .GetInstance(context)
+//            //    .EnqueueUniquePeriodicWork(
+//            //        "total_CO2_23h",
+//            //        ExistingPeriodicWorkPolicy.CancelAndReenqueue,
+//            //        workRequest);
 
-            var workRequest = (PeriodicWorkRequest)new PeriodicWorkRequest.Builder(
-                    Java.Lang.Class.FromType(typeof(DailyWorker)),
-                    24,
-                    Java.Util.Concurrent.TimeUnit.Hours!)
-                .SetInitialDelay((long)delay.TotalMinutes, Java.Util.Concurrent.TimeUnit.Minutes!)
-                .SetConstraints(constraints)
-                .Build();
+//            var workRequest = (PeriodicWorkRequest)new PeriodicWorkRequest.Builder(
+//                    Java.Lang.Class.FromType(typeof(DailyWorker)),
+//                    24,
+//                    Java.Util.Concurrent.TimeUnit.Hours!)
+//                .SetInitialDelay((long)delay.TotalMinutes, Java.Util.Concurrent.TimeUnit.Minutes!)
+//                .SetConstraints(constraints)
+//                .Build();
 
-            WorkManager
-                .GetInstance(context)
-                .EnqueueUniquePeriodicWork(
-                    "total_CO2_23h",
-                    ExistingPeriodicWorkPolicy.Keep,
-                    workRequest);
+//            WorkManager
+//                .GetInstance(context)
+//                .EnqueueUniquePeriodicWork(
+//                    "total_CO2_23h",
+//                    ExistingPeriodicWorkPolicy.Keep,
+//                    workRequest);
 
-        }
-    }
-}
+//        }
+//    }
+//}
