fed5a1614d02d6f21968f6f17df3f0424cf62750
Lauren Keenan <19015831@uhi.ac.uk>
2026-03-26 12:43:56 +0000

    debugs and shrink label to 2 letters

--- src/MauiScreenTime/ViewModels/DashboardViewModel.cs
+++ src/MauiScreenTime/ViewModels/DashboardViewModel.cs
@@ -110,11 +110,24 @@ namespace MauiScreenTime.ViewModels
             Enumerable.Range(0, 10).Select(_ => new BarItem { Height = 0 })
         );
 
+            //DEBUG();
             _ = InitialiseAsync();
             _ = ShowScreenTime();
-            
+            //_ = DisplayLogs();
+
         }
 
+        //public async Task DEBUG()
+        //{
+        //    await _userActivityLogDatabase.DEBUG(200, 0, 0, 0);
+        //    await _userActivityLogDatabase.DEBUG2(350, 0, 0, 0); // 200 TOTAL DAY BEFORE AND 100 TOTAL TODAY 
+        //}
+
+        //public async Task DEBUG1()
+        //{
+        //    await _userActivityLogDatabase.AddActivityLog(0, 0, 66, 0);
+
+        //}
 
         public async Task InitialiseAsync()
         {
@@ -124,6 +137,9 @@ namespace MauiScreenTime.ViewModels
             await ShowScreenTime();
             await ShowCO2e();
 
+            //DEBUG1();
+            //DEBUG();
+
             await GetAndStoreCO2Total();
         }
 
@@ -132,9 +148,24 @@ namespace MauiScreenTime.ViewModels
             await GetUsageData();
             await GetCO2Coversion();
             
+
         }
 
+//        public async Task DisplayLogs()
+//        {
+//            var x = await _userActivityLogDatabase.GetAllActivitiesLogged();
+//            System.Diagnostics.Debug.WriteLine("here items");
 
+//            foreach (var item in x)
+//            {
+//                var xx = System.Text.Json.JsonSerializer.Serialize(item);
+//#if ANDROID
+//                Log.Debug("here", xx);
+//#endif
+//                System.Diagnostics.Debug.WriteLine(xx);
+//            }
+
+//        }
 
         // are these working?
         public Func<List<BarItem>, Color, Color, Task> AnimateBarsAsync { get; set; }
@@ -149,13 +180,13 @@ namespace MauiScreenTime.ViewModels
             
             ScreenTimeData = [.. AppUsageListCO2.Select(obj => new BarItem
             {
-                Label = obj.AppName.Length >= 3 ? obj.AppName[..3] : obj.AppName,
+                Label = obj.AppName.Length >= 3 ? obj.AppName[..2] : obj.AppName,
                 Value = obj.UsageTimeMinutes
             })];
 
             CO2eData = [.. AppUsageListCO2.Select(obj => new BarItem
             {
-                Label = obj.AppName.Length >= 3 ? obj.AppName[..3] : obj.AppName,
+                Label = obj.AppName.Length >= 3 ? obj.AppName[..2] : obj.AppName,
 
                 Value = obj.CO2e
             })];
@@ -389,6 +420,7 @@ namespace MauiScreenTime.ViewModels
                     await Shell.Current.DisplayAlert("Error", "Unable to load data. Please try again.", "OK");
                 }
             }
+
         }
 
         public async Task GetCO2Coversion()
@@ -421,6 +453,10 @@ namespace MauiScreenTime.ViewModels
 
                 await Shell.Current.DisplayAlert("Error", "Unable to total CO2e.", "OK");
             }
+
+
         }
+
+
     }
 }
